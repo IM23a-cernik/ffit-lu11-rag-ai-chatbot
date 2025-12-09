5e829180385bb3364b69be4b13a2d6323fa57489
KrisHD1337 <kriscerni@gmail.com>
2025-12-09 11:41:09 +0100

    Did the test (plenty of Ctrl+C, Ctrl+V)

--- src/main/java/ch/bzz/rag/ApplicationMain.java
+++ src/main/java/ch/bzz/rag/ApplicationMain.java
@@ -2,22 +2,27 @@ package ch.bzz.rag;
 
 import ch.bzz.rag.service.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.document.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.List;
 
 @Slf4j
 @SpringBootApplication
 public class ApplicationMain {
 
     public static void main(String[] args) {
-        String namespaceUrl = "https://wiki.bzz.ch/de/modul/ffit/3-jahr/java/";
-        String regexFilter = "^(?!.*\\/start$).*";
-        boolean overwriteExisting = false;
-
         ConfigurableApplicationContext ctx = SpringApplication.run(ApplicationMain.class, args);
-        WikiCrawlerPipelineService pipeline = ctx.getBean(WikiCrawlerPipelineService.class);
-        pipeline.runPipeline(namespaceUrl, regexFilter, overwriteExisting);
+        StoreService storeService = ctx.getBean(StoreService.class);
+        storeService.updateIndex();
+        int numberOfResults = 5;
+        String query = "Was ist Lombok?";
+        List<Document> docs = storeService.search(query, numberOfResults);
+        for(Document doc : docs){
+            log.info("Doc with id '{}' and metadata '{}'", doc.getId() , doc.getMetadata());
+        }
         ctx.close();
     }
 }
